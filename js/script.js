690e3676a338550c5f0ec1fbcec2fab86b08de3b
xcepti0n <vaibhav.bhatia96@gmail.com>
2018-05-23 02:15:28 +0530

    first day input

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  var show_btn=$('#dance_div');
+  // var show_btn=$('.show-modal');
+  //$("#testmodal").modal('show');
+  
+    show_btn.click(function(){
+       $('#myModal').modal('show');
+  })
+});
