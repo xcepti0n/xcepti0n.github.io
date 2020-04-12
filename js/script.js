9d440d26dcbb4fc867a58d8e818c9470a54e77d2
xcepti0n <vaibhav.bhatia96@gmaiil.com>
2020-04-12 16:38:54 +0530

    Added typed.js usage and social icons

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ $(document).ready(function(){
        $('#myModal').modal('show');
   })
 });
+
+$(function(){
+
+    $("#typed-hobbies").typed({
+        // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+        stringsElement: $('#typed-hobbies-strings'),
+        typeSpeed: 30,
+        backDelay: 300,
+        loop: false,
+        contentType: 'html', // or text
+        // defaults to false for infinite loop
+        loopCount: false,
+        callback: function(){ foo(); },
+        resetCallback: function() { newTyped(); }
+    });
+
+    $(".reset").click(function(){
+        $("#typed").typed('reset');
+    });
+
+});
+function newTyped(){ /* A new typed object */ }
+
+function foo(){ console.log("Callback"); }
+
+
+
